55f88104df28463e4be809facd95791f3d4b13c6
Tomas Vrabel <tomas.vrabel@solarwinds.com>
2021-02-08 21:08:45 +0100

    winform calculator app

---
+++ src/Examples/WinFormDemo/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormDemo
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var a = int.Parse(txtNumber1.Text);
+            var b = int.Parse(txtNumber2.Text);
+
+            txtResult.Text = (a + b).ToString();
+        }
+    }
+}
