dbdf994a95c8baa997b698e925413f46712fad62
Tomas Vrabel <tomas.vrabel@solarwinds.com>
2021-02-09 08:31:40 +0100

    gui exercises

--- src/Examples/WinFormDemo/Calculator.Designer.cs
+++ src/Examples/WinFormDemo/Calculator.Designer.cs
@@ -36,6 +36,7 @@ namespace WinFormDemo
             this.label3 = new System.Windows.Forms.Label();
             this.txtResult = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtNumber1
@@ -97,11 +98,21 @@ namespace WinFormDemo
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Calculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(259, 148);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtResult);
             this.Controls.Add(this.label3);
@@ -125,6 +136,7 @@ namespace WinFormDemo
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtResult;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button button1;
     }
 }
 
